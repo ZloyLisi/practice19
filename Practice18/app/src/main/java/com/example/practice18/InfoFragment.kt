12486c444e0ced158e1371d543c028e03e8ac692
ZloyLisi <github.2005.18@gmail.com>
2023-11-04 13:25:18 +0300

    practice19

---
+++ Practice18/app/src/main/java/com/example/practice18/InfoFragment.kt
@@ -0,0 +1,41 @@
+package com.example.practice18
+
+import androidx.fragment.app.Fragment
+import android.widget.TextView
+import android.os.Bundle
+import android.view.ViewGroup
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+
+
+class InfoFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_info, container, false)
+    }
+    // обновление текстового поля
+    fun setSelectedItem(selectedItem: String?) {
+        val textView = view?.findViewById<TextView>(R.id.textView)
+        val image  = view?.findViewById<ImageView>(R.id.imageView)
+        image?.visibility = View.VISIBLE
+
+        when(selectedItem){
+            getString(R.string.cat1) ->{
+                image?.setImageResource(R.drawable.cat1)
+                textView?.setText(R.string.cat1info)
+            }
+            getString(R.string.cat2) ->{
+                image?.setImageResource(R.drawable.cat2)
+                textView?.setText(R.string.cat2info)
+            }
+            getString(R.string.cat3) ->{
+                image?.setImageResource(R.drawable.cat3)
+                textView?.setText(R.string.cat3info)
+            }
+
+        }
+    }
+}
